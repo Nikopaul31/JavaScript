2bc085f25c7a15d3f25ae4d2b17f9953e85b8f94
paul.bories <paul.bories@gmail.com>
2021-07-01 15:18:55 +0200

    Completed

--- exercice.js
+++ exercice.js
@@ -1,4 +1,18 @@
-function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
+/* It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
+All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty. */
+
+function getAverage(marks) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return Math.floor(marks.reduce(reducer) / marks.length);
+}
+
+module.exports = getAverage;
+
+/* BEST ALTERNATIVE */
+function getAverage(marks) {
+  return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+}
--- exercice.test.js
+++ exercice.test.js
@@ -1,5 +1,9 @@
-const sum = require('./exercice');
+const getAverage = require('./exercice');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('[1,2,3,4] should equal 2', () => {
+  expect(getAverage([1,2,3,4])).toBe(2);
 });
+
+test('[15,15,15] should equal 15', () => {
+    expect(getAverage([15,15,15])).toBe(15);
+  });
